895d8662cebffbbab1c21e9b428c610f35d78dfa
swordfatih <accfldekur@gmail.com>
2022-03-11 12:26:14 +0100

    navigation

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ 
+import Authentification from './pages/Authentification.js';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app de merde !</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Authentification" component={ Authentification } />
+        <Stack.Screen name="Home" component={ HomeScreen } />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
 
 
+
+export default App;
